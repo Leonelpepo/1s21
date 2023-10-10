450193db5aa2334b5c3dac2f0db6efe926a99d45
Leonelpepo <144179245+Leonelpepo@users.noreply.github.com>
2023-10-10 14:52:25 -0600

    Delete p3_1s21 (1) (1).java

--- p3_1s21 (1) (1).java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class P3_1s21{
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        String cadena;
-        System.out.println("Guzman Leal Oscar Leonel");
-        cadena = teclado.nextline();
-    }
-}
