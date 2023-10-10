b7eb4174c98b7ceb0afa457723c09af3b8d14cf2
Leonelpepo <144179245+Leonelpepo@users.noreply.github.com>
2023-10-10 14:37:49 -0600

    oscar leonel guzman leal
    1s21

---
+++ P1/p1_1s21.java
@@ -0,0 +1,6 @@
+class p1_1s21{
+	public static void main(String[] args) {
+	System.out.println("este es nuestro primer programa");
+
+}
+	}
---
+++ P1/p3_1s21.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class P3_1s21{
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        String cadena;
+        System.out.println("Luis Armando Lara Lopez");
+        cadena = teclado.nextline();
+    }
+}
