70750dc2176736c0d715bcc9f96cb348f1711802
Leonelpepo <144179245+Leonelpepo@users.noreply.github.com>
2023-10-20 20:40:31 -0600

    Add files via upload

---
+++ P2/P2p11_1s21.java
@@ -0,0 +1,10 @@
+public class P2p11_1s21{
+	public static void main(String[] args) {
+		for (String cadena : args){
+		   char operacion = cadena.charAt(1);
+		   switch(operacion){
+		   case 'a':System.out.println("inicio a"); break;
+		   case 'b':System.out.println("inicio b"); break;
+		   case 'h':System.out.println("inicio h"); break;
+		   case 'e':System.out.println("inicio e"); break;
+		   
